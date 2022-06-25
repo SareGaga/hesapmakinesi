9949e43c61ecfecc66d7b35d2cbe5ca494da6faa
SareGaga <104974339+SareGaga@users.noreply.github.com>
2022-06-25 16:38:23 +0300

    hafta4

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//buton1 tıklandığında bir fonksiyon icra
+
+document.querySelecetor("#buton1").addEventListener( 'click', function(event){
+    alert("buton 1 tılandı");
+
+});
+
+document.querySelector("#buton1").style.border="2px solid black";
